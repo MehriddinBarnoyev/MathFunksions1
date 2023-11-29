621a9cb3ba3bebad8222586a0da08b691682c595
MehriddinBarnoyev <149578450+MehriddinBarnoyev@users.noreply.github.com>
2023-11-29 13:02:31 +0500

    Math funktions
    Math funktions

---
+++ Math/MathFunctions.java
@@ -0,0 +1,119 @@
+public class MathFunctions {
+    public int sumNumber(int a, int b) {
+
+        return a+b;
+    }
+    public double sumNumber(double a, double b)
+    {
+        return a+b;
+    }
+    public float sumNumber(float a, float b) {
+
+        return a+b;
+    }
+    public long sumNumber(long a, long b) {
+
+        return a+b;
+    }
+    
+
+
+    public float Multipy(float a, float b) {
+
+        return a*b;
+    }
+    public double Multipy(double a, double b) {
+
+        return a*b;
+    }
+    public int Multipyr(int a, int b) {
+
+        return a*b;
+    }
+    public long Multipy(long a, long b) {
+
+        return a*b;
+    }
+
+
+    public long divNumber(long a, long b) {
+
+        return a/b;
+    }
+
+    public float subNumber(float a, float b) {
+
+        return a-b;
+    }
+    public double subNumber(double a, double b) {
+
+        return a-b;
+    }
+    public int subNumber(int a, int b) {
+
+        return a-b;
+    }
+
+    public long subNumber(long a, long b) {
+
+        return a-b;
+    }
+
+    public float divNumber(float a, float b) {
+
+        return a/b;
+    }
+    public double divNumber(double a, double b) {
+
+        return a/b;
+    }
+    public int divNumber(int a, int b) {
+
+        return a / b;
+    }
+
+
+
+    public long absNumber(long a) {
+
+        return Math.abs(a);
+    }
+
+    public float absNumber(float a) {
+
+        return Math.abs(a);
+    }
+    public double absNumber(double a) {
+
+        return Math.abs(a);
+    }
+    public int absNumber(int a) {
+
+        return Math.abs(a);
+    }
+
+
+    public long powNumber(long a) {
+
+        return a * a;
+    }
+
+    public float powNumber(float a) {
+
+        return a * a;
+    }
+    public double powNumber(double a) {
+
+        return a * a;
+    }
+    public int powNumber(int a) {
+
+        return a * a;
+    }
+
+
+
+
+
+
+}
